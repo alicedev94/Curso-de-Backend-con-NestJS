4673ab66dca8abef6e3fe85fb9ef39c215d8e3b3
alicedev94 <alicedev94@gmail.com>
2024-07-28 20:55:52 -0400

    clase 14

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -18,6 +29,22 @@ export class ProductsController {
     @Query('offset') offset = 50,
     @Query('brand') brand: string,
   ): string {
-    return `limit ${limit}, offset ${limit}, brand: ${brand}`;
+    return `limit ${limit}, offset ${offset}, brand: ${brand}`;
+  }
+
+  @Post('create')
+  @HttpCode(HttpStatus.ACCEPTED)
+  create(@Body() payload: any) {
+    return { messege: 'User created', user: payload };
+  }
+
+  @Put('update/:id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return { messege: 'Update user', updateUser: payload };
+  }
+
+  @Delete('delete/:id')
+  delete(@Param('id') id: number) {
+    return { messege: 'Delete user', userId: id };
   }
 }
---
+++ src/services/products/products.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+
+import { Product } from '../../etities/products';
+console.log('Product', Product);
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'phone',
+      description: 'one phone des',
+      price: 100,
+      stock: 10,
+      image: 'image/ruta',
+    },
+  ];
+
+  maxIndex(): number{
+    let arrayIndex = [];
+    this.products.find((item) => {
+      arrayIndex.push(item.id);
+    });
+    return Math.max(...arrayIndex);
+  }
+
+  create(paylaod: Product[]) {
+    let index: number = this.maxIndex();
+    index++;
+    // Añadir la data al arreglo
+    // retornar el nuevo id del producto creado
+  }
+}
